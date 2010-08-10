a76cfc5694ae3500822508ebf3d8ba91cf793da2
jeffcox111 <jeffcox111@localhost>
2010-08-09 22:48:20 -0500

    Updated project files for release 1.3.

--- LaunchLater_LaunchPad/Properties/AssemblyInfo.cs
+++ LaunchLater_LaunchPad/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LLTest")]
+[assembly: AssemblyTitle("LL_LaunchPad")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("LLTest")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LL_LaunchPad")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.24.2221")]
-[assembly: AssemblyFileVersion("1.0.24.2221")]
+[assembly: AssemblyVersion("1.2.35.2243")]
+[assembly: AssemblyFileVersion("1.2.35.2243")]
