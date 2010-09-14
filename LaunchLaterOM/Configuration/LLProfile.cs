1b9a94cfb4288c2c198671c80e80e0a94fba1d77
jeffcox111 <jeffcox111@users.noreply.github.com>
2010-09-13 22:48:50 -0500

    Added individual pause/resume, execute, and cancel commands to the LaunchPad as well as cleaned up 2 bugs where a null timer could have had .Dispose() called on it.

--- LaunchLaterOM/Configuration/LLProfile.cs
+++ LaunchLaterOM/Configuration/LLProfile.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Diagnostics;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace LaunchLaterOM.Configuration
