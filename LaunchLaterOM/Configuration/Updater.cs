9c9597d907a4224bd0d18634a402469bdf70b261
jeffcox111 <jeffcox111@users.noreply.github.com>
2010-09-26 23:29:13 -0500

    Added auto update code, gave LaunchPad the ability to check for updated version and notify user. Added ability to sort apps in the config UI based on name, delay seconds, or enabled status. Changed LaunchPad seconds countdown to be formatted hh:mm:ss.

---
+++ LaunchLaterOM/Configuration/Updater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LinqToFeed;
+
+namespace LaunchLaterOM.Configuration
+{
+    public static class Updater
+    {
+        public static bool UpdateExists(double currentVersion)
+        {
+            if (currentVersion < getLatestReleaseVersion())
+                return true;
+            else
+                return false;
+        }
+
+        private static double getLatestReleaseVersion()
+        {
+            List<Feed> feed = RSSConnector.getBlogInfo("http://launchlater.codeplex.com/Project/ProjectRss.aspx?ProjectRSSFeed=codeplex://release/launchlater");
+
+            string txt = feed.Where(x => x.Title.StartsWith("Released")).First().Title.Split().ToList()[2];
+            double result = 0;
+            double.TryParse(txt, out result);
+            return result;
+ 
+        }
+    }
+}
---
+++ LinqToFeed/FeedAdapters/FeedAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LinqToFeed
+{
+    interface FeedAdapter
+    {
+        List<Feed> getBlogs(XDocument xmlFeed);
+    }
+}
