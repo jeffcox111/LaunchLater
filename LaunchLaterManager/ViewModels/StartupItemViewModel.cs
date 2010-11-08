3a687356758cce9876b845d61b04398de8bc0707
jeffcox111 <jeffcox111@localhost>
2010-11-07 21:05:52 -0600

    Made color theme of import app window match rest of configuration UI. Added application icons to the import app window. Modified the list of importable apps to ignore the LaunchPad. Cleaned up some "using"s.

---
+++ LaunchLaterManager/ViewModels/StartupItemViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+using LaunchLaterOM;
+
+
+namespace LaunchLaterManager.ViewModels
+{
+    public class StartupItemViewModel
+    {
+        public StartupItem StartupApp { get; set; }
+
+        public BitmapImage AppIcon
+        {
+            get
+            {
+                if (StartupApp.FullPath.EndsWith(".exe"))
+                {
+
+                    MemoryStream ms = new MemoryStream();
+                    Icon.ExtractAssociatedIcon(StartupApp.FullPath).ToBitmap().Save(ms, ImageFormat.Png);
+
+                    BitmapImage bi = new BitmapImage();
+                    bi.BeginInit();
+                    bi.StreamSource = ms;
+                    bi.EndInit();
+
+                    return bi;
+                }
+                else
+                {
+                    MemoryStream ms = new MemoryStream();
+                    Icon i = new Icon("settings.ico");
+                    i.ToBitmap().Save(ms, ImageFormat.Png);
+                    BitmapImage bi = new BitmapImage();
+                    bi.BeginInit();
+                    bi.StreamSource = ms;
+                    bi.EndInit();
+
+                    return bi;
+
+                }
+
+
+            }
+
+            private set { }
+
+        }
+    }
+}
