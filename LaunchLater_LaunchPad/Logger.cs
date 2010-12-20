4fa68a168c4102b2a4a4b69e187ddeef142e389e
jeffcox111 <jeffcox111@localhost>
2010-12-20 15:33:34 -0600

    Swallowing exception if there are errors writing to the eventlog in the LaunchPad (this has been seen in Win 7 32bit with UAC enabled).

---
+++ LaunchLater_LaunchPad/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace LaunchLater_LaunchPad
+{
+    public static class Logger
+    {
+        public static void LogInfo(string message)
+        {
+            try
+            {
+                EventLog.WriteEntry("LL_LauchPad", message, EventLogEntryType.Information);
+            }
+            catch { }
+        }
+
+        public static void LogError(string message)
+        {
+            try
+            {
+                EventLog.WriteEntry("LL_LauchPad", message, EventLogEntryType.Error);
+            }
+            catch { }
+        }
+    }
+}
